77f41e172bde6fd7f1b1e6f33bb445e1a0852693
zp06 <108944921+zp06@users.noreply.github.com>
2022-10-19 11:34:29 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,36 +1,54 @@
-let tabuada = 6;
 
-function escreva(){
-    document.write("Tabuada do " + tabuada + "<br>")
-    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
-    
+function total(){
+  let c = document.getElementById("valor").value;
+  let j = document.getElementById("juros").value;
+  let t = document.getElementById("meses").value;
+  let r = 0;
+  for(let i = 1; i <= t; i++){
+      r = c * (1 + (j/100));
+      document.write("Mês " + i + " valor: " + r +"<br>");
+      c = r;
+  }
+  document.write("Resultado: " + r);
 }
-let lista = ["Terra","marte","jupiter"]
 
-function mostraLista(){
-    const newLocal = "Tamanho da lista " + lista.length + "<br>";
-    document.write(newLocal);
-    document.write("planetas " + lista [i] +)
-    for(let i = 0; i < lista.length; i++)
 
+function adicao(){
+let val1 = document.getElementById("v1").value;
+let val2 = document.getElementById("v2").value;
+let r = Number(val1) + Number(val2);
+document.getElementById("resultado").innerHTML =r;
+}
+
+function subtracao(){
+  let val1 = document.getElementById("v1").value;
+  let val2 = document.getElementById("v2").value;
+  let r = Number(val1) - Number(val2);
+  document.getElementById("resultado").innerHTML =r;
+  }
+
+  function divisao(){
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) / Number(val2);
+    document.getElementById("resultado").innerHTML =r;
     }
-}
-function repete(){
-for(let i = 1; i <= 10; i++){
-    document.write("valor do i = " + i "<br>");
-    for(let j = 1; j <=10; j++){
-        document.write(i+" x  "+ j + " = "+(i*j)+"<br>");
+  
+    function multiplicacao(){
+      let val1 = document.getElementById("v1").value;
+      let val2 = document.getElementById("v2").value;
+      let r = Number(val1) * Number(val2);
+      document.getElementById("resultado").innerHTML =r;
+      }
+      function porcentagem(){
+        let val1 = document.getElementById("v1").value;
+        let val2 = document.getElementById("v2").value;
+        let c = 100
+        let p = Number(val2)  / Number(c)
+        let r = Number(val1) * Number(p);
+        document.getElementById("resultado").innerHTML =r;
+        }
+  
 
-    }
-}
-}
+
 
